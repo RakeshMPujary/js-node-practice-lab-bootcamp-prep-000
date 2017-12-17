bb7329ba6582d62605be2abeab6610dcbdedb74e
Rakesh M Pujary <rakeshmpujary@yahoo.com>
2017-12-17 12:52:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// write your code below!
+node// write your code below!
 
 var name = "Joe"
 var height = "74"
@@ -6,5 +6,5 @@ var height = "74"
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
-console.log(height)
+console.log(Height:)
+console.log(parseInt(height)+1)
